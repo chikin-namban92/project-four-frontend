b86a669a34e6de45045a79ce8cef90ddc6727a9a
James Guppy <j.guppy321@gmail.com>
2021-09-12 15:35:57 +0100

    Added homepage image carousel

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
+
+import heroImageOne from '../../assets/hero_caracal_one.jpg'
+import heroImageTwo from '../../assets/hero_caracal_two.jpeg'
+import heroImageThree from '../../assets/hero_caracal_three.jpg'
+import heroImageFour from '../../assets/hero_caracal_four.jpg'
 
 function Home() {
   return (
-    <h1>This is the Home Page</h1>
+    <>
+      <section className="hero-slideshow">
+        <div>
+          <AliceCarousel autoPlay autoPlayInterval="3000">
+            <img src={heroImageOne} className="sliderimg" alt="caracal"/>
+            <img src={heroImageTwo} className="sliderimg" alt="caracal"/>
+            <img src={heroImageThree} className="sliderimg" alt="caracal"/>
+            <img src={heroImageFour} className="sliderimg" alt="caracal"/>
+          </AliceCarousel>
+        </div>
+      </section>
+    </>
+    
+
   )
 }
 
