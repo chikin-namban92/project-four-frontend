62de7013b62d3b6b8d8e52c1009bb76949842d5e
James Guppy <j.guppy321@gmail.com>
2021-10-05 21:56:41 +0100

    Minor edits, removing unused code

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,9 +4,6 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        {/* <p>
-          <strong>Find Floppas</strong> by James Guppy
-        </p> */}
         <ul>
           <li>{`© ${new Date().getFullYear()} Find Floppas`}</li>
         </ul>
